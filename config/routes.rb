049bf5d6192950132815469fa77a41628e8e6599
vagrant <vagrant@rails-dev-box>
2014-06-29 16:00:47 +0000

    Added a controller to Posts and showed index page of posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Bloccit::Application.routes.draw do
-  get "welcome/index"
+	resources :posts
+
+
+  	get 'about'=> 'welcome#about'
   
-  get "welcome/about"
-  
-  root to: 'welcome#index'
+  	root to: 'welcome#index'
 end
