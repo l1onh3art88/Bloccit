945e352adcfa198dc5d58c66311b70f46c49f80c
vagrant <vagrant@rails-dev-box>
2014-07-07 23:40:06 +0000

    Implementing voting

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Bloccit::Application.routes.draw do
   resources :users, only: [:update]
 
   resources :topics do
-  	resources :posts, except: [:index] do 
+    resources :posts, except: [:index] do
       resources :comments, only: [:create, :destroy]
+      get '/up-vote' => 'votes#up_vote', as: :up_vote
+      get '/down-vote' => 'votes#down_vote', as: :down_vote
     end
   end
 
