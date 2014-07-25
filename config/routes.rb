d1e1b8726e4ce2188cf16e63023879c325c2dfaa
vagrant <vagrant@rails-dev-box>
2014-07-25 19:13:57 +0000

    Popular Posts, and Users navigation tabs

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Bloccit::Application.routes.draw do
 
   devise_for :users
-  resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
 
   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-      resources :favorites, only: [:create, :destroy]
-      resources :likes, only: [:create, :destroy]
-      
-    end
+    resources :posts, except: [:index], controller: 'topics/posts' 
   end
+  resources :posts, only: [:index] do
+    resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
+  end
+  
+
   	get 'about'=> 'welcome#about'
   
   	root to: 'welcome#index'
