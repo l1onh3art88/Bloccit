9dada834a2d6ad5920ffd7f1fa325aa96c209a27
vagrant <vagrant@rails-dev-box>
2014-09-13 16:53:43 +0000

    finishing touches

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,6 @@
-redis_conf = YAML::load(File.read(Rails.root.join("config", "redis.yml")))[Rails.env]
-REDIS = Redis.new(redis_conf)
+if Rails.env.production?
+  uri = URI.parse(ENV["REDISTOGO_URL"]) 
+  REDIS = Redis.new(:host => uri.host , :port => uri.port, :password => uri.password)
+else
+  REDIS = Redis.new
+end
