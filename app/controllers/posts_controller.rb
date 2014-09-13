d166c54b2abf97fdd17063689b431f5ec869542d
vagrant <vagrant@rails-dev-box>
2014-09-13 22:01:22 +0000

    finished product 1.2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  
+  before_filter :authenticate_user!, only: [:index]
    def index
      @posts = Post.visible_to(current_user).where("posts.created_at > ?", 7.days.ago).paginate(page: params[:page], per_page: 10)
    end
