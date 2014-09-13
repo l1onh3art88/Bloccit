c45396fe66f8aac0baf3930264d312ecada8a6cc
vagrant <vagrant@rails-dev-box>
2014-09-13 21:57:39 +0000

    finished product 1.1

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,5 @@
 class TopicsController < ApplicationController
-   before_filter :authenticate_user!, only: [:index]
+   before_filter :authenticate_user!, only: [:show]
   def index
   	@topics = Topic.visible_to(current_user).paginate(page: params[:page], per_page: 10)
   	authorize @topics
